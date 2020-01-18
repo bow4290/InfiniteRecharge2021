dbe54ad2e5b1c979d65622aeafa250c615156604
PranavNarala1 <pranavnarala@gmail.com>
2020-01-18 11:35:19 -0500

    Added smartdashboard import.

--- BOWRobot-master/src/main/java/frc/robot/Robot.java
+++ BOWRobot-master/src/main/java/frc/robot/Robot.java
@@ -10,6 +10,7 @@ package frc.robot;
 import com.revrobotics.ColorSensorV3;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -66,6 +67,15 @@ public class Robot extends TimedRobot
 
         Color detectedColor = m_colorSensor.getColor();
         System.out.println("The color is " + detectedColor.toString());
+
+        double IR = m_colorSensor.getIR();
+
+        SmartDashboard.putNumber("Red", detectedColor.red);
+        SmartDashboard.putNumber("Green", detectedColor.green);
+        SmartDashboard.putNumber("Blue", detectedColor.blue);
+        SmartDashboard.putNumber("IR", IR);
+//        SmartDashboard.putNumber("Proximity", proximity);
+        // yet another comment
     }
 
     /**
