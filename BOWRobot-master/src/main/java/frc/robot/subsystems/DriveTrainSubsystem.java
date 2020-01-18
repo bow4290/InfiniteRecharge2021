30a544581500796645d4cd46bbb3f6c7d1c2e7e7
Frederick Cosby <frederickacosby@gmail.com>
2020-01-18 13:24:48 -0500

    "worked on color system code a little, added color subsystem, WPM 0135"

--- BOWRobot-master/src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
+++ BOWRobot-master/src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -7,15 +7,15 @@ import frc.robot.commands.SparkDriveCommand;
 
 public class DriveTrainSubsystem extends SubsystemBase {
     private final Spark leftSpark1;
-    private final Spark leftSpark2;
+//    private final Spark leftSpark2;
     private final Spark rightSpark1;
-    private final Spark rightSpark2;
+//    private final Spark rightSpark2;
 
     public DriveTrainSubsystem() {
         leftSpark1 = new Spark(1);
-        leftSpark2 = new Spark(5);
+//        leftSpark2 = new Spark(0);
         rightSpark1 = new Spark(0);
-        rightSpark2 = new Spark(3);
+//        rightSpark2 = new Spark(0);
         setDefaultCommand(new SparkDriveCommand(this));
     }
 
@@ -28,9 +28,9 @@ public class DriveTrainSubsystem extends SubsystemBase {
         System.out.println("Left value is "+leftspeed);
         System.out.println("Right value is "+rightspeed);
         leftSpark1.setSpeed(leftspeed);
-        leftSpark2.setSpeed(leftspeed);
+//        leftSpark2.setSpeed(leftspeed);
         rightSpark1.setSpeed(rightspeed);
-        rightSpark2.setSpeed(rightspeed);
+//        rightSpark2.setSpeed(rightspeed);
     }
 
 }
