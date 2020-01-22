133ad83bfd17f9bfad93f53ce6d6f87681b71ced
PranavNarala1 <pranavnarala@gmail.com>
2020-01-21 19:24:47 -0500

    I changed the robot port locations to robot.java.

--- BOWRobot-master/src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
+++ BOWRobot-master/src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -13,13 +13,14 @@ public class DriveTrainSubsystem extends SubsystemBase {
     private final Spark rightSpark2;
     private final Spark rightSpark3;
 
-    public DriveTrainSubsystem() {
-        leftSpark1 = new Spark(1);
-        leftSpark2 = new Spark(5);
-        leftSpark3 = new Spark(2);
-        rightSpark1 = new Spark(0);
-        rightSpark2 = new Spark(3);
-        rightSpark3 = new Spark(4);
+    public DriveTrainSubsystem(int leftSpark1Channel, int leftSpark2Channel, int leftSpark3Channel,
+                               int rightSpark1Channel, int rightSpark2Channel, int rightSpark3Channel) {
+        leftSpark1 = new Spark(leftSpark1Channel);
+        leftSpark2 = new Spark(leftSpark2Channel);
+        leftSpark3 = new Spark(leftSpark3Channel);
+        rightSpark1 = new Spark(rightSpark1Channel);
+        rightSpark2 = new Spark(rightSpark2Channel);
+        rightSpark3 = new Spark(rightSpark3Channel);
         setDefaultCommand(new SparkDriveCommand(this));
     }
 
