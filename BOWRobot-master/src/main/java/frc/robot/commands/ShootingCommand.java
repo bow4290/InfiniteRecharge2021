1c82230c40e3660c8ddaf0e70ab5abdc1106012c
Frederick Cosby <frederickacosby@gmail.com>
2020-01-21 20:47:49 -0500

    "shooting subsystem and command"

---
+++ BOWRobot-master/src/main/java/frc/robot/commands/ShootingCommand.java
@@ -0,0 +1,27 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.subsystems.ShootingSubsystem;
+
+import java.util.Set;
+
+public class ShootingCommand extends CommandBase {
+
+    private ShootingSubsystem shootingSubsystem;
+    private final Set<Subsystem> subsystems;
+
+    public ShootingCommand(ShootingSubsystem shootingSubsystem) {
+        this.shootingSubsystem = shootingSubsystem;
+        this.subsystems = Set.of(shootingSubsystem);
+    }
+
+    public void execute() {
+        shootingSubsystem.shootBall(.65);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        shootingSubsystem.shootBall(0);
+    }
+}
---
+++ BOWRobot-master/src/main/java/frc/robot/subsystems/ShootingSubsystem.java
@@ -0,0 +1,25 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+import com.revrobotics.SparkMax;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ShootingSubsystem extends SubsystemBase {
+    private final CANSparkMax leftShooter;
+    private final CANSparkMax rightShooter;
+
+    private static final int leftDeviceID = 0;
+    private static final int rightDeviceID = 1;
+
+    public ShootingSubsystem() {
+        leftShooter = new CANSparkMax(leftDeviceID, CANSparkMaxLowLevel.MotorType.kBrushless);
+        rightShooter = new CANSparkMax(rightDeviceID, CANSparkMaxLowLevel.MotorType.kBrushless);
+    }
+
+    public void shootBall(double shooterSpeed) {
+        leftShooter.set(shooterSpeed);
+        rightShooter.setInverted(true);
+        rightShooter.set(shooterSpeed);
+    }
+}
