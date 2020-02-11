45a5c6e56274884d092986bee098488bd943b03d
adrian <adrianmanaure512@gmail.com>
2020-02-11 18:43:04 -0500

    color sensor command before color stop

--- BOWRobot-master/src/main/java/frc/robot/commands/ColorSensorCommand.java
+++ BOWRobot-master/src/main/java/frc/robot/commands/ColorSensorCommand.java
@@ -3,6 +3,7 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.subsystems.ColorSensorSubsystem;
+import frc.robot.subsystems.WheelSpinnerSubsystem;
 
 import java.util.Set;
 
@@ -10,7 +11,8 @@ public class ColorSensorCommand extends CommandBase {
 
     public ColorSensorSubsystem colorSensorSubsystem;
     public final Set<Subsystem> subsystems;
-
+    WheelSpinnerSubsystem colorWheelSpinner;
+    int colorCount;
     /**
      * this establishes the color sensor subsystem
      * @param colorSensorSubsystem
@@ -21,10 +23,20 @@ public class ColorSensorCommand extends CommandBase {
         this.subsystems = Set.of(colorSensorSubsystem);
     }
 
-    public void execute(){
-
+    public void wheelSpin(){
+        do {
+            if (colorSensorSubsystem.getColor() == colorSensorSubsystem.getkRedTarget()){
+                colorCount++;
+            }
+        } while (colorCount <7);
     }
 
+//    public void stopOnColor(){
+//        if(Robot.getBooleanProperty() == 'B'){
+//
+//        }
+//
+//    }
     @Override
     public boolean isFinished() {
         return false;
@@ -34,5 +46,8 @@ public class ColorSensorCommand extends CommandBase {
     public Set<Subsystem> getRequirements() {
 
         return this.subsystems;
+    }
+    public void execute(){
+
     }
 }
